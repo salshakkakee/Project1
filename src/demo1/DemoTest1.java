a9af68a595d79f577939e514230b2a7297f52578
salsh <salsh@Sally.lan>
2025-01-29 21:12:37 -0500

    adding more data

--- src/demo1/DemoTest1.java
+++ src/demo1/DemoTest1.java
@@ -3,6 +3,11 @@ package demo1;
 public class DemoTest1 {
 	public static void main(String[] args) {
 		System.out.println("Java");
+		
+		char gender = 'F';
+		String name = "Sally";
+		
+		System.out.println(gender + name);
 	}
 
 }
